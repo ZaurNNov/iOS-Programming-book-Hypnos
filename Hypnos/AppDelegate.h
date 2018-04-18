fe658cabec59d99c9d32083fac128cd1b095ea46
Zaur Giyasov <zagiyas@mera.ru>
2018-04-18 13:42:49 +0300

    Initial Commit

---
+++ Hypnos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnos
+//
+//  Created by Zaur Giyasov on 18/04/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
