3d8b6fe8a9a7521c0b352a3c6aadbd25a05f75fe
Zaur Giyasov <zagiyas@mera.ru>
2018-04-20 14:39:32 +0300

    erase old comments!

--- Hypnos/HypnosisView.m
+++ Hypnos/HypnosisView.m
@@ -17,31 +17,19 @@
 
 @implementation HypnosisView
 
-
 - (void)drawRect:(CGRect)rect {
     // Drawing code
     CGRect bounds = self.bounds;
     
     // Figure out the center of the bounds rectangle
     CGPoint center = CGPointMake((bounds.origin.x + bounds.size.width / 2), (bounds.origin.y + bounds.size.height / 2));
-    
-    
-    // Radius for figure
-    /* // Drawing concentric circles
-    CGFloat radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
-     */
+
     // The largest
     CGFloat maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
-    
     // UIBezier - rect
     UIBezierPath *path = [[UIBezierPath alloc]init];
-    
-    /* // Drawing concentric circles
-    // Add an arc
-    // from 0 to 2*Pi
-    [path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
-     */
+
     for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
         [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
         [path addArcWithCenter:center
@@ -58,33 +46,11 @@
     
     // draw line with path
     [path stroke];
-    
-    //////////////////////////////////////
-    // Bronze Challenge: Draw an Image
-    /*
-    
-    UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
-    // Image bounds - is 1/4 origin bounds
-    CGRect logoImageRect;
-    logoImageRect.origin.x = bounds.origin.x + bounds.size.width / 4;
-    logoImageRect.origin.y = bounds.origin.y + bounds.size.height / 4;
-    
-    // Image size = 1/2 origin bounds
-    logoImageRect.size.width = bounds.size.width / 2;
-    logoImageRect.size.height = bounds.size.height / 2;
-    
-    [logoImage drawInRect:logoImageRect];
-     */
-    //////////////////////////////////////
-    
-    //////////////////////////////////////
-    // Gold Challenge: Shadows and Gradients
 }
 
 -(instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // all override
         self.backgroundColor = [UIColor clearColor];
         self.circleColor = [UIColor lightGrayColor];
     }
@@ -94,8 +60,6 @@
 
 // When a finger touches the screen
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-//    NSLog(@"touched");
-    
     // Get 3 random numders for RGB color
     CGFloat red = (arc4random() % 100) / 100.0;
     CGFloat green = (arc4random() % 100) / 100.0;
