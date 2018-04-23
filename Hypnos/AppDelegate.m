4f850b57e863df87adaf8ccbf8a1c739b5491723
Zaur Giyasov <zagiyas@mera.ru>
2018-04-23 10:03:10 +0300

    скролл теперь работает.

--- Hypnos/AppDelegate.m
+++ Hypnos/AppDelegate.m
@@ -18,27 +18,19 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-        // Override point for customization after application launch.//Generic way to make root view controller;
     self.window.rootViewController = [[UIViewController alloc] init];
     
-    // 1
-    /* //bounds vs. frame
-    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
-     */
-    
     // 5 Add ScrollView
-    /*
     CGRect firstFrame = self.window.bounds;
     HypnosisView *firstView = [[HypnosisView alloc] initWithFrame:firstFrame];
     //[self.window addSubview:firstView]; //touch not work
     [self.window.rootViewController.view addSubview:firstView]; //touch work!!!
-     */
     
     // Create CGRects for frames
     CGRect screenRect = self.window.bounds;
     CGRect bigRect = screenRect;
     bigRect.size.width *= 2.0;
-    //bigRect.size.height *= 2.0;
+//    bigRect.size.height *= 2.0;
     
     // Create a screen-sized scroll view and it to the window
     UIScrollView *scrollView = [[UIScrollView alloc]initWithFrame:screenRect];
@@ -55,47 +47,12 @@
     HypnosisView *anotherView = [[HypnosisView alloc]initWithFrame:screenRect];
     [scrollView addSubview:anotherView];
 
-    // Tell the scroll view how big its content area is
-//    scrollView.contentSize = bigRect.size;
+     // Tell the scroll view how big its content area is
+    scrollView.contentSize = bigRect.size;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
-    
-//    // 2
-//    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
-//    HypnosisView *secondView = [[HypnosisView alloc]initWithFrame:secondFrame];
-//    secondView.backgroundColor = [UIColor brownColor];
-////    [self.window addSubview:secondView];
-//    [firstView addSubview:secondView];
 }
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 
 @end
